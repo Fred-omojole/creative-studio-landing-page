6e78c2ba302a21733aada4b8cf9ec1abd5e2d9bf
freddy.dev <omoyeleomojole2004@gmail.com>
2025-01-08 18:02:06 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     description:
       "Showcasing a visually captivating and conversion-focused landing page inspired by Inflatable. Perfect for brands aiming to enhance their digital presence.",
     url: "https://creative-studio-landing-page.vercel.app/",
-    siteName: "Inflatable Landing Page",
+    siteName: "creative-studio-landing-page.vercel.app",
     images: [
       {
         url: "https://github.com/Fred-omojole/creative-studio-landing-page/blob/master/public/images/inflatable-17.png?raw=true", // Create a custom Open Graph image (1200x630 recommended)
@@ -33,16 +33,29 @@ export const metadata: Metadata = {
     type: "website",
   },
   twitter: {
-    card: "summary_large_image", // Ensures a large preview card for Twitter
+    card: "summary_large_image",
     title: "Inflatable | Modern Landing Page Design",
     description:
       "Experience a cutting-edge landing page inspired by Inflatable. Designed to captivate users and drive engagement for your brand.",
     images: [
       "https://github.com/Fred-omojole/creative-studio-landing-page/blob/master/public/images/inflatable-17.png?raw=true",
-    ], // Match the Open Graph image
+    ],
   },
-  viewport: "width=device-width, initial-scale=1", // Essential for responsive design
-  themeColor: "#1A202C", // Set a theme color matching the branding of your page
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
 };
 
 export default function RootLayout({
